881dd8dc2ac065b5cc7945bcc2348fc1cb6e2a4c
sekouremy@gmail.com <sekouremy@gmail.com@a6724ec2-1828-344f-a6e4-dcee8d45cc22>
2013-05-31 15:45:30 +0000

    Initial test of creation of Catkin workspace

--- trunk/ros_gazebo/install.sh
+++ trunk/ros_gazebo/install.sh
@@ -13,7 +13,13 @@ mkdir ros-stacks
 echo "source /opt/ros/groovy/setup.bash" >> ~/.bashrc
 echo "export ROS_PACKAGE_PATH=/home/$USER/ros-stacks:\$ROS_PACKAGE_PATH" >> ~/.bashrc
 
-#should include the catkin workspace here soon!
+#Create catkin workspace (untested)
+# http://www.ros.org/wiki/catkin/Tutorials/create_a_workspace
+mkdir ~/ros-stacks/catkin_ws
+mkdir ~/ros-stacks/catkin_ws/src
+cd ~/ros-stacks/catkin_ws/src
+catkin_init_workspace
+cd ~
 
 ##install gazebo
 #sudo apt-get -y install build-essential libtinyxml-dev libtbb-dev libxml2-dev libqt4-dev pkg-config  libprotoc-dev libfreeimage-dev libprotobuf-dev protobuf-compiler libboost-all-dev freeglut3-dev cmake libogre-dev libtar-dev libcurl4-openssl-dev libcegui-mk2-dev
