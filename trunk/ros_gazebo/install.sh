3d6e095d9c92ef8addfe983a3daebe5a47e7a0b9
sekouremy@gmail.com <sekouremy@gmail.com@a6724ec2-1828-344f-a6e4-dcee8d45cc22>
2014-03-10 19:49:08 +0000

    Added changes for catkinized ros_webservice

--- trunk/ros_gazebo/install.sh
+++ trunk/ros_gazebo/install.sh
@@ -5,23 +5,18 @@ sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu raring main" > /etc/apt
 
 wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
 sudo apt-get -y update
-sudo apt-get -y install ros-hydro-desktop
+#install ROS with STAGE and GAZEBO
+sudo apt-get -y install ros-hydro-stage-ros
+sudo apt-get -y install ros-hydro-gazebo-ros-control ros-hydro-gazebo-ros-pkgs
 sudo apt-get -y install xvfb
 sudo apt-get -y install gdb
 sudo apt-get -y install python-rosdistro python-rosdep python-rosinstall python-rosinstall-generator
 sudo apt-get -y install ros-hydro-mjpeg-server
 
-#install STAGE
-sudo apt-get -y install ros-hydro-stage-ros
-
-echo "source /opt/ros/hydro/setup.bash" >> ~/.bashrc
-source ~/.bashrc
-sudo apt-get -y install python-webpy
-
-#install gazebo
-sudo apt-get -y install ros-hydro-gazebo-ros-control ros-hydro-gazebo-ros-pkgs
 sudo rosdep init
 rosdep update
+
+sudo apt-get -y install python-webpy
 
 #create catkin workspace
 mkdir -p ~/catkin_ws/src
@@ -30,11 +25,12 @@ catkin_init_workspace
 cd ~/catkin_ws
 catkin_make
 echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
+echo "source /opt/ros/hydro/setup.bash" >> ~/.bashrc
+source ~/.bashrc
 
 #install ROS web service manually
-#mkdir ~/ros-stacks
-#cd ~/ros-stacks/
-#svn co https://remy-robotics-tools.googlecode.com/svn/trunk/ros_web_service/
-#rosmake ros_web_service
-##this library isn't catkin ready as yet!
-
+cd ~/catkin_ws/src/
+svn co https://remy-robotics-tools.googlecode.com/svn/trunk/ros_web_service/
+cd ~/catkin_ws/
+catkin_make
+source ~/catkin_ws/devel/setup.bash
