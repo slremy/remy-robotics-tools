cc298c22cbf1bac496ac0010fbc4b41727bad91d
sekouremy@gmail.com <sekouremy@gmail.com@a6724ec2-1828-344f-a6e4-dcee8d45cc22>
2015-01-20 18:55:01 +0000

    ROS Quadrotor with a Camera!!

---
+++ trunk/ros_morse/ros_quadrotor.py
@@ -0,0 +1,51 @@
+from morse.builder import *
+import math
+
+# choose either 'attitude', 'velocity' or 'waypoint' for control
+#control = 'attitude'
+control = 'velocity'
+#control = 'waypoint'
+
+
+# must be called before creating component
+bpymorse.set_speed(fps=60, logic_step_max=5, physics_step_max=5)
+
+# Simple quadrotor with rigid body physics
+quadrotor = Quadrotor()
+quadrotor.translate(x=-1.2483, y=1.7043, z=1.8106)
+quadrotor.name = 'mav'
+
+if 'attitude' in control:
+    # simple controller taking RC-like roll/pitch/yaw/thrust input
+    motion = RotorcraftAttitude()
+    motion.name = 'attitude'
+    motion.add_stream('ros', 'morse.middleware.ros.read_asctec_ctrl_input.CtrlInputReader')
+elif 'velocity' in control:
+    motion = RotorcraftVelocity()
+    motion.name = 'velocity'
+    # read a Twist message
+    motion.add_stream('ros')
+else:
+    motion = RotorcraftWaypoint()
+    motion.name = 'waypoint'
+    # read a Pose message
+    motion.add_stream('ros')
+
+quadrotor.append(motion)
+camera = VideoCamera()
+camera.translate(x=0.2, z=0.9)
+camera.add_stream('ros')
+quadrotor.append(camera)
+
+imu = IMU()
+imu.name = 'imu'
+# IMU with z-axis down (NED)
+imu.rotate(x=math.pi)
+imu.add_stream('ros')
+quadrotor.append(imu)
+
+env = Environment('indoors-1/indoor-1')
+env.show_framerate(True)
+#env.show_physics(True)
+
+env.create()
