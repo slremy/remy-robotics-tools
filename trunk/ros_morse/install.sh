e2dbe0c0ada33e9fa4d2b68ed5eddfc564a7c367
sekouremy@gmail.com <sekouremy@gmail.com@a6724ec2-1828-344f-a6e4-dcee8d45cc22>
2015-01-20 19:27:54 +0000

    Updating the source list information

--- trunk/ros_morse/install.sh
+++ trunk/ros_morse/install.sh
@@ -1,6 +1,6 @@
 #install ROS
 sudo cp /etc/apt/sources.list  /etc/apt/sources.list.backup
-sudo sed -i "/^# deb.*multiverse/ s/^# //" /etc/apt/sources.list
+sudo sed -i "/^# deb.*multiverse/ s/^# //" /etc/apt/sources.list 
 #instructions for 14.04 (Trusty)
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu trusty main" > /etc/apt/sources.list.d/ros-latest.list'
 wget https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -O - | sudo apt-key add -
