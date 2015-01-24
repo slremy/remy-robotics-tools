1daf35491f4adeeaa9ae2b7ef239ec7a46a262e9
sekouremy@gmail.com <sekouremy@gmail.com@a6724ec2-1828-344f-a6e4-dcee8d45cc22>
2015-01-24 14:52:59 +0000

    Initial check-in of code to install the ball on plate plant

---
+++ trunk/controls_web_service/installplate.sh
@@ -0,0 +1,3 @@
+sudo apt-get -y install python-webpy python-numpy
+wget https://remy-robotics-tools.googlecode.com/svn/trunk/controls_web_service/ballplate.py
+wget https://remy-robotics-tools.googlecode.com/svn/trunk/controls_web_service/index2.html
