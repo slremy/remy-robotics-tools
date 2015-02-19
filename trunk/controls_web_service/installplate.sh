28b0bdd5ef6a7fd7a71d8753468a7b9a63668384
sekouremy@gmail.com <sekouremy@gmail.com@a6724ec2-1828-344f-a6e4-dcee8d45cc22>
2015-02-19 14:07:19 +0000

    Including the necessary update.

--- trunk/controls_web_service/installplate.sh
+++ trunk/controls_web_service/installplate.sh
@@ -1,3 +1,4 @@
+sudo apt-get -y update
 sudo apt-get -y install python-webpy python-numpy
 wget https://remy-robotics-tools.googlecode.com/svn/trunk/controls_web_service/ballplate.py
 wget https://remy-robotics-tools.googlecode.com/svn/trunk/controls_web_service/index2.html
